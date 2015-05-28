454ef6a6bc1511d7385bcf2579629737add91382
benobab <benlacroix83@gmail.com>
2015-05-28 22:11:15 +0200

    Bootstrap Files

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8"/>
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Benjamin Lacroix</title>
+</head>
+<body>
+    <header class="container col-md-12">
+        <h1>Benjamin Lacroix - ESCPE Lyon</h1>
+        <nav class="pull-right row">
+            <ul class="nav nav-pills">
+                <button>Accueil</button>
+                <button>Compétences</button>
+                <button>Expériences proffessionnelels</button>
+                <button>Exemples de projet</button>
+                <button >Contact</button>
+            </ul>
+        </nav>
+    </header>
+    <div class="jumbotron milieu col-md-12 row">
+        <div class="col-md-8 pull-left">
+            <p>
+                Ici toute l'info du milieu
+            </p>
+        </div>
+        <div class="col-md-3 pull-right">
+            <form class="form-horizontal">
+                <div class="form-group">
+                    <label for="email">Email address:</label>
+                    <input type="email" class="form-control" id="email">
+                </div>
+                <div class="form-group">
+                    <label for="pwd">Password:</label>
+                    <input type="password" class="form-control" id="pwd">
+                </div>
+                <div class="checkbox">
+                    <label><input type="checkbox"> Remember me</label>
+                </div>
+                <div class="row">
+                    <button type="submit" class="btn btn-default">Submit</button>
+                    <button type="submit" class="btn btn-default">S'enregistrer</button>
+                </div>
+            </form>
+        </div>
+    </div>
+    <footer class="container col-md-12 navbar-fixed-bottom">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse dolor nisl, interdum ac ullamcorper sed, pretium ut dolor. Fusce pulvinar magna eu enim finibus, eget vestibulum ipsum blandit. Proin in eros diam. Aliquam auctor mollis volutpat. Maecenas id est justo. Praesent eget sapien et ligula lobortis mollis. Suspendisse augue ipsum, mattis eu neque ac, consectetur volutpat sapien. Nam at sagittis lacus. Proin sit amet augue ut ipsum posuere pharetra. Aenean sed mauris mollis, cursus libero in, hendrerit nunc.
+
+            In quis accumsan metus. Suspendisse vel nisl a turpis cursus pulvinar in eget nibh. Fusce facilisis velit dui, id laoreet nisl dictum non. Interdum et malesuada fames ac ante ipsum primis in faucibus. Ut sodales tellus ac dignissim molestie. Donec quis tortor ligula. Sed vel pellentesque risus. Vestibulum vel nibh sit amet est pellentesque rutrum sed a ex. Pellentesque venenatis ac tortor at porttitor. Vestibulum aliquet tortor vel justo euismod dapibus.
+
+        </p>
+    </footer>
+</body>
+
